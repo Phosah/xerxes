a888f1c320cf62ae45a743df3e3c8896e67e5865
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-03-05 02:11:33 -0800

    update to account and network component

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex items-center justify-center w-screen h-screen opacity-25">
-        <div class="max-w-4xl mx-auto p-4 bg-brand-black-1">
-            <div class="mb-4 text-right">
+    <div class="flex items-center justify-center">
+        <div class="max-w-4xl mx-auto p-4 bg-brand-black-1 rounded-md">
+            <div class="mb-4 flex justify-end">
                 <svg
                     class="w-6 h-6"
                     fill="currentColor"
@@ -15,24 +15,49 @@
                     />
                 </svg>
             </div>
-            <h4 class="mb-4 text-2xl">Account</h4>
+            <h4 class="mb-4 text-lg">Account</h4>
             <div
-                class="flex items-center justify-between space-x-4 bg-gradient-to-tr from-brand-blue-1 to-brand-blue-2"
+                class="flex items-center justify-between space-x-10 p-4 bg-gradient-to-tr from-brand-blue-2 to-brand-blue-1 rounded-md"
             >
                 <div>
                     <img src="@/assets/account-photo.png" alt="Account photo" />
                 </div>
                 <div>
-                    <p class="font-neue-machina-light">Balance</p>
-                    <h4 class="text-2xl">$780,650.98</h4>
+                    <p class="font-neue-machina-light text-sm text-[#EDF4FF]">Balance</p>
+                    <h4 class="text-lg">$780,650.98</h4>
                 </div>
                 <div>
-                    <p class="font-neue-machina-light">Wallet</p>
-                    <h4 class="text-2xl">Wallet Connect</h4>
+                    <p class="font-neue-machina-light text-sm text-[#EDF4FF]">Wallet</p>
+                    <h4 class="text-lg">Wallet Connect</h4>
                 </div>
                 <div>
-                    <p class="font-neue-machina-light">Address</p>
-                    <h4 class="text-2xl">0xC8efkd839dkd99ke</h4>
+                    <p class="font-neue-machina-light text-sm text-[#EDF4FF]">Address</p>
+                    <h4 class="text-lg">0xC8efkd839dkd99ke</h4>
+                </div>
+            </div>
+            <div class="mt-8 p-4 rounded-md bg-[#282529]">
+                <div class="flex items-center justify-between pb-6 border-b border-[#343541]">
+                    <div class="flex items-center space-x-4">
+                        <div>
+                            <img class="w-6" src="@/assets/metamask-logo.png" alt="Metamask" />
+                        </div>
+                        <p class="font-neue-machina-regular">7 Networks Connected</p>
+                    </div>
+                    <svg
+                        width="10"
+                        height="5"
+                        viewBox="0 0 10 5"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M9.16675 4.66699L5.00008 0.500325L0.833415 4.66699H9.16675Z"
+                            fill="white"
+                        />
+                    </svg>
+                </div>
+                <div v-for="(network, ix) in networkArr" :key="ix">
+                    <Network :network="network" />
                 </div>
             </div>
         </div>
@@ -40,5 +65,59 @@
 </template>
 
 <script setup>
+import { ref } from "vue"
+import Network from './Network.vue';
+import ethereum1 from "@/assets/ethereum-logo-1.png"
+import arbitrium from "@/assets/arbitrium-logo.png"
+import ethereum2 from "@/assets/ethereum-logo-2.png"
+import ethereum3 from "@/assets/ethereum-logo-3.png"
+import cronos from "@/assets/cronos-logo.png"
+import binance from "@/assets/binance-logo.png"
+import energi from "@/assets/energi.png"
+
+const networkArr = ref([
+    {
+        logo: ethereum1,
+        alt: "Ethereum",
+        name: "Ethereum",
+        value: "$56,000.80"
+    },
+    {
+        logo: cronos,
+        alt: "Cronos",
+        name: "Cronos",
+        value: "$56,000.80"
+    },
+    {
+        logo: binance,
+        alt: "Binance SC",
+        name: "Binance Smart Chain",
+        value: "$56,000.80"
+    },
+    {
+        logo: energi,
+        alt: "Energi",
+        name: "Energi",
+        value: "$56,000.80"
+    },
+    {
+        logo: arbitrium,
+        alt: "Arbitrium",
+        name: "Arbitrium",
+        value: "$56,000.80"
+    },
+    {
+        logo: ethereum2,
+        alt: "Ethereum",
+        name: "Ethereum",
+        value: "$56,000.80"
+    },
+    {
+        logo: ethereum3,
+        alt: "Ethereum",
+        name: "Ethereum",
+        value: "$56,000.80"
+    },
+])
 
 </script>
---
+++ src/views/Network.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex items-center justify-between mt-5">
+        <div class="flex items-center space-x-4">
+            <div>
+                <img :src="network.logo" :alt="network.alt" />
+            </div>
+            <div class="flex items-center space-x-2 text-sm">
+                <p>{{ network.name }} {{ (network.value) }}</p>
+                <img src="@/assets/expand.png" alt="Expand logo" />
+            </div>
+        </div>
+        <div>
+            <svg
+                class="w-6 h-6 text-[#5FBE91]"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                />
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    network: {
+        type: Object
+    }
+})
+</script>
